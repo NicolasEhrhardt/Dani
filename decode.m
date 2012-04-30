6c1d9ffaba98287113ffd80fa7bc6b8872cc2e1c
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2012-05-01 01:42:43 +0200

    display triangulation

--- decode.m
+++ decode.m
@@ -1,1 +1,22 @@
-Hello World
+%structure of figure
+figure = struct('p', {}, 'col', {})
+
+r = 5
+%size of image : 2^r
+figure(1).p = [-2^(r-1); 2^(r-1)]
+figure(1).col = 0
+
+figure(2).p = [2^(r-1); 2^(r-1)]
+figure(2).col = 255
+
+figure(3).p = [2^(r-1); -2^(r-1)]
+figure(3).col = 0
+
+figure(4).p = [-2^(r-1); -2^(r-1)]
+figure(4).col = 255
+
+
+p = [figure(:).p]
+
+tri = DelaunayTri(p')
+triplot(tri)
